027172995bfb1473e0ea8b1d2caf00f66eef467f
Nectar <nectar.296@gmail.com>
2026-03-14 17:32:32 +0700

    Chỉnh lại UI cho form Login + dọn dẹp tí code

---
+++ WinformQLSV/Login.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Winform1
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bt_login = new System.Windows.Forms.Button();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.txt_usr = new System.Windows.Forms.TextBox();
+            this.lb2 = new System.Windows.Forms.Label();
+            this.lb1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.bt_login);
+            this.splitContainer1.Panel2.Controls.Add(this.txt_pass);
+            this.splitContainer1.Panel2.Controls.Add(this.txt_usr);
+            this.splitContainer1.Panel2.Controls.Add(this.lb2);
+            this.splitContainer1.Panel2.Controls.Add(this.lb1);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(762, 478);
+            this.splitContainer1.SplitterDistance = 254;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(254, 478);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(405, 32);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Phần mềm quản lý Sinh Viên";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // bt_login
+            // 
+            this.bt_login.BackColor = System.Drawing.SystemColors.Control;
+            this.bt_login.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.bt_login.Location = new System.Drawing.Point(223, 295);
+            this.bt_login.Name = "bt_login";
+            this.bt_login.Size = new System.Drawing.Size(132, 30);
+            this.bt_login.TabIndex = 9;
+            this.bt_login.Text = "Đăng nhập";
+            this.bt_login.UseVisualStyleBackColor = false;
+            this.bt_login.Click += new System.EventHandler(this.bt_login_Click);
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(210, 240);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(176, 22);
+            this.txt_pass.TabIndex = 8;
+            // 
+            // txt_usr
+            // 
+            this.txt_usr.Location = new System.Drawing.Point(210, 193);
+            this.txt_usr.Name = "txt_usr";
+            this.txt_usr.Size = new System.Drawing.Size(176, 22);
+            this.txt_usr.TabIndex = 7;
+            this.txt_usr.TextChanged += new System.EventHandler(this.txt_usr_TextChanged_1);
+            // 
+            // lb2
+            // 
+            this.lb2.AutoSize = true;
+            this.lb2.Location = new System.Drawing.Point(143, 243);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(61, 16);
+            this.lb2.TabIndex = 6;
+            this.lb2.Text = "Mật khẩu";
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Location = new System.Drawing.Point(106, 196);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(98, 16);
+            this.lb1.TabIndex = 5;
+            this.lb1.Text = "Tên đăng nhập";
+            this.lb1.Click += new System.EventHandler(this.lb1_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 478);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button bt_login;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.TextBox txt_usr;
+        private System.Windows.Forms.Label lb2;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinformQLSV/Login.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform1
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void bt_login_Click(object sender, EventArgs e)
+        {
+            string username = txt_usr.Text.Trim();
+            string pass = txt_pass.Text.Trim();
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(pass))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ tài khoản và mật khẩu", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+
+            using (var db = new DataClasses1DataContext())
+            {
+                bool tinChuan = db.TaiKhoans.Any(tk =>
+                    tk.TenDangNhap == username && tk.MatKhau == pass);
+
+                if (tinChuan)
+                {
+                    Main mainForm = new Main();
+                    mainForm.FormClosed += (s, args) => this.Close(); // Đóng cả form Login khi Main đóng
+                    mainForm.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Sai tên tk hoặc mật khẩu", "Lỗi",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txt_pass.Clear();
+                    txt_pass.Focus();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txt_pass.UseSystemPasswordChar = true; // Ẩn mật khẩu khi nhập
+        }
+
+
+
+        private void txt_usr_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lb1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
